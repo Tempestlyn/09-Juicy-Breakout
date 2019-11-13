eb8defc6fbe1ddd6fffcdeb8c2998139f5ce130c
jwringer <jwringer@iu.edu>
2019-11-13 02:24:28 -0500

    Fixed animations and updated readme

--- Breakout-master/Scripts/Ball.gd
+++ Breakout-master/Scripts/Ball.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 onready var Game = get_node("/root/Game")
 onready var Starting = get_node("/root/Game/Starting")
 onready var Particles = get_node("/root/Game/Ball/Particles1")
+onready var Paddle = get_node("/root/Game/Paddle")
 var hits = 0;
 func _ready():
 	contact_monitor = true
@@ -11,13 +12,16 @@ func _ready():
 func _physics_process(delta):
 	# Check for collisions
 	var bodies = get_colliding_bodies()
-	
+	var animation = Paddle.get_node("AnimationPlayer")
 	for body in bodies:
 		if body.is_in_group("Tiles"):
 			Game.change_score(body.points)
 			hits += 1
 			body.queue_free()
-			
+		if body == Paddle:
+			if hits > 2:
+				animation.play("ChargeHit")	
+			hits = 0
 		
 	
 	if position.y > get_viewport().size.y + 100:
@@ -25,7 +29,13 @@ func _physics_process(delta):
 		Starting.startCountdown(3)
 		queue_free()
 		
-	if hits > 1:
+	if hits < 3:
+		Particles.process_material.scale_random = 0.0
+	if hits > 2:
 		Particles.amount = 2.0
-		
+		Particles.process_material.scale_random = 0.5		
+	elif hits > 4:
+		Particles.amount = 3.0
+		Particles.process_material.scale_random = 1.0	
+	
 	print(hits)
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # 09-Juicy-Breakout
 
-I gave the game an 'electric' aesthetic, and created new sprites, sounds, and particles 
-effects to achieve this. 
+I gave the game an 'electric' aesthetic, and created new sprites and particles 
+effects to achieve this. I tried to implement sound using an audio player, but I couldn't
+get it to work. 
+
+Detroying bricks increases the 'charge' of the ball. When it hits the paddle when its
+charge is great enough, it will play a discharge animation.
